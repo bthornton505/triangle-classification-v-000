f5d4aa599508a06111a5610ce44039055cebd0c7
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-27 17:05:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,7 @@ class Triangle
   end 
   
   def kind 
+    validate_triangle
     if a == b && b == c 
       :equilateral 
     elsif a == b || a == c || b == c 
@@ -22,7 +23,6 @@ class Triangle
     [a, b, c].each {|s| real_triangle << false if s <= 0}
     raise TriangleError if real_triangle.include?(false)
   end 
-  
   
   class TriangleError < StandardError 
   end 
